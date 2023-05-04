894fc483b15beca0d2db0cc25a7c991647b6d1b1
Elianc22 <chavezelian2000@gmail.com>
2023-05-04 17:11:35 +0200

    displaying the list of products

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import productos from './mocks/products.json'
+import { Products } from './components/Products'
+
 function App () {
   return (
-    <h1>Shopping Cart</h1>
+    <Products products={productos.products} />
   )
 }
-
 export default App
