44edcfc0456a89eb768aa84082a5c78ce98e6f8b
Elianc22 <chavezelian2000@gmail.com>
2023-05-05 18:35:09 +0200

    I created the filtersContext

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ import productos from './mocks/products.json'
 import { Products } from './components/Products'
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { useFilters } from './hooks/useFilters'
 
 function App () {
   const [products] = useState(productos.products)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === 'all' || product.category === filters.category)
-      )
-    })
-  }
-
+  const { filterProducts } = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
+      <Footer />
     </>
   )
 }
