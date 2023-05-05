e5fb4d09e3bde18d4dce487dec6b1741bda25b08
Elianc22 <chavezelian2000@gmail.com>
2023-05-05 12:57:16 +0200

    adding price and category filters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import productos from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
+import { Header } from './components/Header'
 
 function App () {
+  const [products] = useState(productos.products)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === 'all' || product.category === filters.category)
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
-    <Products products={productos.products} />
+    <>
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
+    </>
   )
 }
 export default App
